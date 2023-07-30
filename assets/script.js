677dd4a4e6e74ad762e5b2054dd408b3884e6f3e
Quick Brown Fox <jeremiah.n.richardson@gmail.com>
2023-07-30 18:54:20 -0400

    added question 1 page element function for ordered list

--- assets/script.js
+++ assets/script.js
@@ -1,6 +1,15 @@
 //Creating a function to allow for the starting content to appear and disappear
 var timerEl = document.querySelector(".time");
-var startScreenDiv = document.querySelector("div");
+var startScreenDiv = document.querySelector(".main");
+
+function startClear () {
+    document.querySelector("h1").remove();
+    document.querySelector("p").remove();
+    var startButton = document.getElementById("startbutton")
+    startButton.remove();
+    quest1();
+}
+
 function startPage() {
     timerEl.textContent = "Time: 0";
     var header = document.createElement("h1");
@@ -11,10 +20,33 @@ function startPage() {
     startScreenDiv.appendChild(explanation);
     var startButton = document.createElement("button");
     startButton.textContent = "Start Quiz";
+    startButton.setAttribute("id", "startbutton");
     startScreenDiv.appendChild(startButton);
+    //Inspired by https://www.w3schools.com/jsref/tryit.asp?filename=tryjsref_element_addeventlistener2
+    var myBtn = document.getElementById("startbutton");
+    myBtn.addEventListener("click", startClear);
 }
 startPage();
 
+function quest1 () {
+    var header = document.createElement("h1");
+    header.textContent = "Commonly used data types DO Not Include:";
+    startScreenDiv.appendChild(header);
+    var multiChoiceBin = document.createElement("ol");
+    startScreenDiv.appendChild(multiChoiceBin);
+    var multiChoice1 = document.createElement("li");
+    multiChoice1.textContent = "strings";
+    multiChoiceBin.appendChild(multiChoice1);
+    var multiChoice2 = document.createElement("li");
+    multiChoice2.textContent = "booleans";
+    multiChoiceBin.appendChild(multiChoice2);
+    var multiChoice3 = document.createElement("li");
+    multiChoice3.textContent = "alerts";
+    multiChoiceBin.appendChild(multiChoice3);
+    var multiChoice4 = document.createElement("li");
+    multiChoice4.textContent = "numbers";
+    multiChoiceBin.appendChild(multiChoice4);
+}
 
 //Adding stored value for penalty and starting timer value
 var startValue = 0;
